3399b412d9ba6a32d2aba24e1290f738ac028408
Molly :) <mollyflanagan4@outlook.com>
2023-11-13 13:21:45 +0000

    ResourceItem component created

---
+++ app/components/ResourceItem.js
@@ -0,0 +1,10 @@
+export default function ResourceItem() {
+  return(
+    <div className="my-2 px-8 py-4">
+      <div>
+        <h2 className="font-bold">HMRC Data Extract</h2>
+        <p className="text-gray-600 text-sm">How to resolve hmrc data extract</p>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,19 +1,27 @@
+import ResourceItem from "./components/ResourceItem";
+
 export default function Home() {
   return (
-   <main className="bg-white md:max-w-2xl mx-auto md:shadow-lg md:rounded-lg md:mt-8 overflow-hidden">
-    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 p-8">
+   
+   <main className="bg-white md:max-w-3xl mx-auto md:shadow-lg md:rounded-lg md:mt-8 overflow-hidden">
+    <div className="bg-gradient-to-r from-emerald-400 to-green-400 p-8">
       <h1 className="font-bold text-xl">Thaler Guide</h1>
       <p className="text-opacity-90 text-slate-700">A Repository for Thaler Resources</p>
     </div>
-
-    <div className="bg-gray-100 px-8 py-2 flex">
+    
+    <div className="bg-gray-100 px-8 py-4 flex border-b"> 
       <div className="grow"></div>
       <div>
-        <button className="bg-blue-500 py-1 px-4 rounded-md text-white">
+        <button className="bg-emerald-600 py-1 px-4 rounded-md text-white">
         Create
         </button>
       </div>
     </div>
+
+    <ResourceItem />
+    <ResourceItem />
+    <ResourceItem />
+    
    </main>
   )
 }
