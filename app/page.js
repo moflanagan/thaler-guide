46e1ece5215b97efe5649ef857fc0e0b7fc2d020
Molly :) <mollyflanagan4@outlook.com>
2023-11-13 12:40:48 +0000

    Creating the header

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+   <main className="bg-white md:max-w-2xl mx-auto md:shadow-lg md:rounded-lg md:mt-8 overflow-hidden">
+    <div className="bg-gradient-to-r from-cyan-400 to-blue-400 p-8">
+      <h1 className="font-bold text-xl">Thaler Guide</h1>
+      <p className="text-opacity-90 text-slate-700">A Repository for Thaler Resources</p>
+    </div>
+   </main>
+  )
+}
